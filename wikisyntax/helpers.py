c63ab8d611d70ccbed3bab2be1d105bf5a0a2bf9
yeago <subsume@gmail.com>
2012-07-01 21:47:43 -0700

    cleaned up, formulating some useful backends

--- wikisyntax/helpers.py
+++ wikisyntax/helpers.py
@@ -1,133 +1,109 @@
 import re
 
+from django.contrib.markup.templatetags.markup import markdown
 from django.conf import settings
 from django.db.models.loading import get_model
-from django.core.cache import cache
 from django.utils.safestring import mark_safe
+from wikisyntax import fix_unicode
 
 def wikisafe_markdown(value):
-	from django.contrib.markup.templatetags.markup import markdown
-	return mark_safe(markdown(value.replace('[[','LBRACK666').replace(']]','RBRACK666')).replace('LBRACK666','[[').replace('RBRACK666',']]'))
+    LSAFETY = 'LBRACK666' # Some unlikely nonsense
+    RSAFETY = 'RBRACK666' # Some unlikely nonsense
+    value = value.replace('[[', LSAFETY).replace(']]', RSAFETY)
+    value = markdown(value)
+    value = value.replace(LSAFETY,'[[').replace(RSAFETY,']]')
+    return mark_safe(value)
 
 class WikiException(Exception): # Raised when a particular string is not found in any of the models.
-	pass
+    pass
 
 def wikify(match): # Excepts a regexp match
-	wikis = [] # Here we store our wiki model info
+    wikis = [] # Here we store our wiki model info
 
-	for i in settings.WIKISYNTAX:
-		name = i[0]
-		modstring = i[1]
-		module = __import__(".".join(modstring.split(".")[:-1]))
-		for count, string in enumerate(modstring.split('.')):
-			if count == 0:
-				continue
+    for name, modstring in settings.WIKISYNTAX:
+        module = __import__(".".join(modstring.split(".")[:-1]))
+        for count, string in enumerate(modstring.split('.')):
+            if count == 0:
+                continue
 
-			module = getattr(module,string)
+            module = getattr(module,string)
+        module.name = name
+        wikis.append(module())
 
-		module.name = name
-		wikis.append(module())
+    token, trail = match.groups() # we track the 'trail' because it may be a plural 's' or something useful
 
-	token, trail = match.groups() # we track the 'trail' because it may be a plural 's' or something useful
+    if ':' in token:
+        """
+        First we're checking if the text is attempting to find a specific type of object.
 
-	if ':' in token:
-		"""
-		First we're checking if the text is attempting to find a specific type of object.
+        Exmaples:
 
-		Exmaples:
+        [[user:Subsume]]
 
-		[[user:Subsume]]
+        [[card:Jack of Hearts]]
 
-		[[card:Jack of Hearts]]
+        """
+        prefix, name = token.split(':',1)
+        for wiki in wikis:
+            if prefix == wiki.name:
+                if wiki.attempt(name,explicit=True):
+                    """
+                    We still check attempt() because maybe
+                    work is done in attempt that render relies on,
+                    or maybe this is a false positive.
+                    """
+                    return wiki.render(name,trail=trail,explicit=True)
+                else:
+                    raise WikiException
 
-		"""
-		prefix = token.split(':',1)[0].lower().rstrip()
-		name = token.split(':',1)[1].rstrip()
-		for wiki in wikis:
-			if prefix == wiki.name:
-				if wiki.attempt(name,explicit=True):
-					"""
-					We still check attempt() because maybe
-					work is done in attempt that render relies on,
-					or maybe this is a false positive.
-					"""
-					return wiki.render(name,trail=trail,explicit=True)
-				else:
-					raise WikiException
+    """
+    Now we're going to try a generic match across all our wiki objects.
 
-	"""
-	Now we're going to try a generic match across all our wiki objects.
+    Example:
 
-	Example:
+    [[Christopher Walken]]
 
-	[[Christopher Walken]]
+    [[Studio 54]]
+    [[Beverly Hills: 90210]] <-- notice ':' was confused earlier as a wiki prefix name
 
-	[[Studio 54]]
-	[[Beverly Hills: 90210]] <-- notice ':' was confused earlier as a wiki prefix name
+    [[Cat]]s <-- will try to match 'Cat' but will include the plural 
 
-	[[Cat]]s <-- will try to match 'Cat' but will include the plural 
+    [[Cats]] <-- will try to match 'Cats' then 'Cat'
 
-	[[Cats]] <-- will try to match 'Cats' then 'Cat'
+    """
+    for wiki in wikis:
+        if getattr(wiki,'prefix_only',None):
+            continue
 
-	"""
-	for wiki in wikis:
-		if getattr(wiki,'prefix_only',None):
-			continue
+        if wiki.attempt(token):
+            return wiki.render(token,trail=trail)
 
-		if wiki.attempt(token):
-			return wiki.render(token,trail=trail)
+    """
+    We tried everything we could and didn't find anything.
+    """
 
-	"""
-	We tried everything we could and didn't find anything.
-	"""
-
-	raise WikiException("No item found for '%s'"% (token))
+    raise WikiException("No item found for '%s'"% (token))
 
 class wikify_string(object):
-	def __call__(self, string, fail_silently=True):
-		self.fail_silently = fail_silently
-		self.cache = {}
-		self.set_cache = {}
+    def __call__(self, string, fail_silently=True):
+        self.fail_silently = fail_silently
+        WIKIBRACKETS = '\[\[([^\]]+?)\]\]'
+        if not string:
+            return ''
 
-		from wikisyntax import fix_unicode
-		WIKIBRACKETS = '\[\[([^\]]+?)\]\]'
-		if not string:
-			return ''
+        string = fix_unicode.fix_unicode(string)
+        content = re.sub('%s(.*?)' % WIKIBRACKETS,self.markup_to_links,string)
+        return content
 
-		string = fix_unicode.fix_unicode(string)
+    def __new__(cls, string, **kwargs):
+        obj = super(wikify_string, cls).__new__(cls)
+        return obj(string, **kwargs)
 
-		if getattr(settings,'WIKISYNTAX_DISABLE_CACHE',False) == False:
-			keys = re.findall(WIKIBRACKETS, string)
-			self.cache = cache.get_many([k.replace(' ','-').lower() for k in keys if len(k) < 251])
-
-		content = re.sub('%s(.*?)' % WIKIBRACKETS,self.markup_to_links,string)
-		cache.set_many(self.set_cache)
-		return content
-
-	def __new__(cls, string, **kwargs):
-		obj = super(wikify_string, cls).__new__(cls)
-		return obj(string, **kwargs)
-
-	def markup_to_links(self,match):
-		string = match.groups()[0].lower().replace(' ','-')
-
-		if getattr(settings,'WIKISYNTAX_DISABLE_CACHE',False) == False:
-			if string in self.cache:
-				return self.cache[string]
-
-			if string in self.set_cache:
-				return self.set_cache[string] # Maybe they typed it twice?
-
-		try:
-			new_val = wikify(match)
-
-			if getattr(settings,'WIKISYNTAX_DISABLE_CACHE',False) == False:
-				self.set_cache[string] = new_val
-
-			return new_val
-
-		except WikiException:
-			if not self.fail_silently:
-				raise
-
-			return string
+    def markup_to_links(self,match):
+        string = match.groups()[0].lower().replace(' ','-')
+        try:
+            return wikify(match)
+        except WikiException:
+            if not self.fail_silently:
+                raise
+            return string
--- wikisyntax/templatetags/wiki_tags.py
+++ wikisyntax/templatetags/wiki_tags.py
@@ -8,47 +8,47 @@ register = template.Library()
 @register.filter
 @template.defaultfilters.stringfilter
 def wikimarkdown(value):
-	return wikisafe_markdown(value)
+    return wikisafe_markdown(value)
 
 class WikiFormat(template.Node):
-	def __init__(self, string):
-		self.string = string
+    def __init__(self, string):
+        self.string = string
 
-	def build_string(self, context):
-		return self.string.resolve(context)
+    def build_string(self, context):
+        return self.string.resolve(context)
 
-	def process_string(self, string):
-		string = wikisafe_markdown(string)
-		string = wikify_string(string)
-		if len(string.split("</p>")) == 2 and string.split("</p>")[1] == "":
-			string = string.replace("<p>","").replace("</p>","")
+    def process_string(self, string):
+        string = wikisafe_markdown(string)
+        string = wikify_string(string)
+        if len(string.split("</p>")) == 2 and string.split("</p>")[1] == "":
+            string = string.replace("<p>","").replace("</p>","")
 
-		return string
+        return string
 
-	def render(self, context):
-		string = self.build_string(context)
-		string = self.process_string(string)
+    def render(self, context):
+        string = self.build_string(context)
+        string = self.process_string(string)
 
-		#content = re.sub('(.*?)(?:(?:\r\n\r\n)*$|\r\n\r\n)','<p>%s</p>\r\n' % r'\1' , content)
-		return string.replace("[[","").replace("]]","")
+        #content = re.sub('(.*?)(?:(?:\r\n\r\n)*$|\r\n\r\n)','<p>%s</p>\r\n' % r'\1' , content)
+        return string.replace("[[","").replace("]]","")
 
 class WikiBlockFormat(WikiFormat):
-	def process_string(self,string):
-		"""
-		Its not generally safe to use markdown on a whole blocktag because the block
-		may contain html already and there's no telling how nice it will play.
-		"""
-		string = wikify_string(string)
-		return string
+    def process_string(self,string):
+        """
+        Its not generally safe to use markdown on a whole blocktag because the block
+        may contain html already and there's no telling how nice it will play.
+        """
+        string = wikify_string(string)
+        return string
 
-	def build_string(self,context):
-		return self.string.render(context)
+    def build_string(self,context):
+        return self.string.render(context)
 
 @register.tag
 def wikify(parser, token):
-	tag_name, var = token.split_contents()
-	string = template.Variable(var)
-	return WikiFormat(string)
+    tag_name, var = token.split_contents()
+    string = template.Variable(var)
+    return WikiFormat(string)
 
 """
 
@@ -56,6 +56,6 @@ def wikify(parser, token):
 
 @register.tag
 def wikiblock(parser, token):
-	nodelist = parser.parse(('endwikiblock',))
-	parser.delete_first_token()
-	return WikiBlockFormat(nodelist)
+    nodelist = parser.parse(('endwikiblock',))
+    parser.delete_first_token()
+    return WikiBlockFormat(nodelist)
