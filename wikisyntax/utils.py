32b177c815961fea3dbea94697f5436379159207
Ignacio Vaggione <ivaggione@gmail.com>
2015-10-30 00:08:08 -0300

    Remove unnecessary iter

--- wikisyntax/utils.py
+++ wikisyntax/utils.py
@@ -2,8 +2,8 @@ from wikisyntax.constants import LEFTBRACKET, RIGHTBRACKET
 
 
 def balanced_brackets(value):
-    len_lbrack = len([i for i in value.split(LEFTBRACKET)])
-    len_rbrack = len([i for i in value.split(RIGHTBRACKET)])
+    len_lbrack = len(value.split(LEFTBRACKET))
+    len_rbrack = len(value.split(RIGHTBRACKET))
     if len_lbrack != len_rbrack:
         return False
     return True
