7597c2a28bbc6f2dd4f8bf05e2d1c2bf7bfa70fe
yeago <yeago999@gmail.com>
2017-04-04 15:20:48 -0700

    name->string

--- wikisyntax/admin.py
+++ wikisyntax/admin.py
@@ -4,7 +4,7 @@ from wikisyntax.models import Blob
 
 
 class BlobAdmin(admin.ModelAdmin):
-    search_fields = ['name']
+    search_fields = ['string']
     list_display = ['string']
     list_filter = ['prefix']
 
